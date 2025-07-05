7689106b42a3327b46128d089677973f9f7d36f8
momoelabdi <momo.elabdi@bluewin.ch>
2025-07-05 13:15:47 +0200

    add eks dev

--- envs/dev/infra/dev.tf
+++ envs/dev/infra/dev.tf
@@ -1,10 +1,16 @@
 
 
 module "eks" {
-    source = "terraform-modules/eks"
+    source = "../../../terraform-modules/eks"
+    cluster_name = var.cluster_name
+    environment = var.environment
+    subnet_ids = var.subnet_ids # network module outpout
+    eks_node_group_instance_types = var.eks_node_group_instance_types
+    eks_node_group_desired_size = var.eks_node_group_desired_size
+    eks_node_group_max_size = var.eks_node_group_max_size
+    eks_node_group_min_size = var.eks_node_group_min_size
 }
 
-
-module "networking" {
-    source = "terraform-modules/networking"
-}
+# module "network" {
+#     source = "terraform-modules/networking"
+# }
