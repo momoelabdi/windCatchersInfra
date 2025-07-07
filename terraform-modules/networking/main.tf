16da15d973a24d7f3fcfa334b358d7ee0c1db025
momoelabdi <momo.elabdi@bluewin.ch>
2025-07-07 20:46:00 +0200

    aws vpc for eks

---
+++ terraform-modules/networking/main.tf
@@ -0,0 +1,57 @@
+# -> VPC for eks cluster 
+resource "aws_vpc" "eks_vpc" {
+  cidr_block = var.cidr_block
+  tags = {
+    Name = "eksVPC"
+  }
+}
+
+data "aws_availability_zones" "azs" {
+  state = "available"
+}
+
+# -> worker nodes private subnets 
+resource "aws_subnet" "private" {
+  count             = var.count_private_subnet
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = cidrsubnet(var.cidr_block, 8, count.index)
+  availability_zone = data.aws_availability_zones.azs.names[count.index]
+  tags = {
+    Name                                        = "eks-private-subnet-${count.index + 1}"
+    "kubernetes.io/role/internal-elb"           = "1"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.eks_vpc.id
+  tags   = { Name = "eks-nodes-igw" }
+}
+
+
+resource "aws_eip" "eip" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.eip.id
+  subnet_id     = aws_subnet.public[0].id
+  tags = {
+    Name = "eks-nodes-ngw"
+  }
+}
+
+# -> worker nodes public subnets
+resource "aws_subnet" "public" {
+  count             = var.count_public_subnet
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = cidrsubnet(var.cidr_block, 8, count.index + var.count_private_subnet)
+  availability_zone = data.aws_availability_zones.azs.names[count.index]
+  tags = {
+    Name                                        = "eks-public-subnet-${count.index + 1}"
+    "kubernetes.io/role/elb"                    = "1"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+  }
+}
+
+
