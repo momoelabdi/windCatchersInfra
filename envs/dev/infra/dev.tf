166b560177b0cca84051105c1c66b01f3cebbd8e
momoelabdi <momo.elabdi@bluewin.ch>
2025-07-08 14:29:52 +0200

    add LB controller to eks

--- envs/dev/infra/dev.tf
+++ envs/dev/infra/dev.tf
@@ -1,16 +1,28 @@
 
 
 module "eks" {
-    source = "../../../terraform-modules/eks"
-    cluster_name = var.cluster_name
-    environment = var.environment
-    subnet_ids = var.subnet_ids # network module outpout
-    eks_node_group_instance_types = var.eks_node_group_instance_types
-    eks_node_group_desired_size = var.eks_node_group_desired_size
-    eks_node_group_max_size = var.eks_node_group_max_size
-    eks_node_group_min_size = var.eks_node_group_min_size
+  source                        = "../../../terraform-modules/eks"
+  cluster_name                  = var.cluster_name
+  environment                   = var.environment
+  subnet_ids                    = module.network.private_subnets_ids
+  eks_node_group_instance_types = var.eks_node_group_instance_types
+  eks_node_group_desired_size   = var.eks_node_group_desired_size
+  eks_node_group_max_size       = var.eks_node_group_max_size
+  eks_node_group_min_size       = var.eks_node_group_min_size
 }
 
-# module "network" {
-#     source = "terraform-modules/networking"
-# }
+
+module "network" {
+  source       = "../../../terraform-modules/networking"
+  cluster_name = var.cluster_name
+}
+
+module "loadbalancer" {
+  source       = "../../../terraform-modules/loadbalancer"
+  cluster_name = var.cluster_name
+  environment                   = var.environment
+  cluster_ca_certificate = module.eks.cluster_ca_certificate
+  eks_oidc_provider_arn = module.eks.eks_oidc_provider_arn
+  cluster_endpoint = module.eks.cluster_endpoint
+  vpc_id = module.network.eks_vpc_id
+}
